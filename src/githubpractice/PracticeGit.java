003b448190deab3d964e4c3021c2ec8e83920d66
suryaprakash9893 <suryaprakasha@live.in>
2023-07-05 21:11:43 -0400

    Git Practice

---
+++ src/githubpractice/PracticeGit.java
@@ -0,0 +1,13 @@
+package githubpractice;
+
+public class PracticeGit {
+
+	public PracticeGit() {
+		int git;
+	}
+	
+	void tryGit () {
+		System.out.println("Practice git status add commit push");
+	}
+
+}
---
+++ src/githubpractice/PracticeMain.java
@@ -0,0 +1,14 @@
+package githubpractice;
+
+public class PracticeMain {
+
+	public static void main(String[] args) {
+		//Instantiating a class.
+		PracticeGit repo = new PracticeGit();
+		
+		//calling a function
+		repo.tryGit();
+
+	}
+
+}
