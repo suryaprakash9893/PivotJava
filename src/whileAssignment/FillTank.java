0582ed9df022e72b2e1c063e6605e750c0359475
suryaprakash9893 <suryaprakasha@live.in>
2023-07-12 08:13:15 -0400

    LoopsAssignment-Water_Tank_Build_3

--- src/whileAssignment/FillTank.java
+++ src/whileAssignment/FillTank.java
@@ -1,9 +1,26 @@
 package whileAssignment;
 
+import java.util.Scanner;
+
 public class FillTank {
 	double currentTankLevel,maxFillCapacity,fillRate,maxTankCapacity;
+	Scanner water = new Scanner(System.in);
 
 	void measureTank() {
+		System.out.println("Enter Max Tank capacity=");
+		maxTankCapacity=water.nextDouble();
+		
+		System.out.println("Enter Max Fill capacity=");
+		maxFillCapacity=water.nextDouble();
+		
+		if (maxFillCapacity < maxTankCapacity) {
+			
+			System.out.println("Enter Fill Rate=");
+			fillRate=water.nextDouble();
+
+			System.out.println("Enter current water level=");
+			currentTankLevel = water.nextDouble();
+			
 		while (currentTankLevel <= maxFillCapacity) {
 
 			currentTankLevel += fillRate;
@@ -14,5 +31,8 @@ public class FillTank {
 			}
 			System.out.println("Added "+fillRate+" L water from bucket.Current waterlevel in the tank is " + currentTankLevel + "L");
 		}
+	}else {
+		System.out.println("Safety level should be less than the Max capacity");
 	}
+}
 }
--- src/whileAssignment/TankMain.java
+++ src/whileAssignment/TankMain.java
@@ -8,23 +8,6 @@ public class TankMain {
 //Instantiating a class. or create an object "FillLevel"
 
 		FillTank fillLevel = new FillTank();
-		Scanner waterlevel = new Scanner(System.in);
-		
-		System.out.println("Enter Max Tank capacity=");
-		fillLevel.maxTankCapacity=waterlevel.nextDouble();
-		
-		System.out.println("Enter Max Fill capacity=");
-		fillLevel.maxFillCapacity=waterlevel.nextDouble();
-		
-		System.out.println("Enter Fill Rate=");
-		fillLevel.fillRate=waterlevel.nextDouble();
-
-		System.out.println("Enter current water level=");
-		fillLevel.currentTankLevel = waterlevel.nextDouble();
-		
-		
-		
-		
 		fillLevel.measureTank();
 	}
 
