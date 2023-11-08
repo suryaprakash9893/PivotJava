939095f328d0579a78f5204e2e5b30277839aafc
Vindhuja2023 <vindhuja.rajeev@gmail.com>
2023-11-07 21:59:27 -0500

    git commands added in githubpractise package

--- src/githubpractice/PracticeGit.java
+++ src/githubpractice/PracticeGit.java
@@ -6,5 +6,11 @@ public class PracticeGit {
 	void tryGit () {
 		System.out.println("Practice git status add commit push");
 	}
+	
+	
+	public void sampleMethod() {
+		System.out.println("Grid commands : git status,git init,git add src/,git commit -m,git push,git --version,git log,git diff,git branch");
+	
+	}
 
 }
